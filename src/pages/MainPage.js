2f6ae52c22663a25cb8b84a946e466a46b81774d
shoney <ddmczp@naver.com>
2025-01-12 10:33:15 +0900

    feat: BasicLayout 사용

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout"
+
 const MainPage = () => {
-  return <div className="text-3xl font-extrabold m-3 p-3">MainPage</div>;
+    return (
+        <BasicLayout>
+            <div className="text-3xl font-extrabold m-3 p-3">
+                Welcome to My Website
+            </div>
+            <div className="flex">
+                <Link to="/about">About 페이지로 이동</Link>
+            </div>
+        </BasicLayout>
+    );
 };
 
 export default MainPage;
