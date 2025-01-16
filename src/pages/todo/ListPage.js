d4fd9bb26036faa7c52c75957b94ce6697a166bb
shoney <ddmczp@naver.com>
2025-01-16 13:08:09 +0900

    feat: ListPage useSearchParams( ) 로 쿼리스트링 받도록 추가

--- src/pages/todo/ListPage.js
+++ src/pages/todo/ListPage.js
@@ -1,9 +1,13 @@
-import { Component } from "react"
-
+import { useSearchParams } from "react-router-dom";
 const ListPage = () => {
+    const [searchParams] = useSearchParams();
+
+    const page = searchParams.get("page") ? searchParams.get("page") : 1;
+    const size = searchParams.get("size") ? searchParams.get("size") : 10;
+
     return (
         <div>
-            Todo List Component
+            Todo List Component {page} - {size}
         </div>
     )
 }
